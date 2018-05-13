7b20a8962d02bbadbc63c5afc553a721282b860a
Maxim Konovalov <maxim.konovalov@onefactor.com>
2018-05-14 00:00:15 +0300

    Prototyping for structure data collection and writing. Init example Java + Spring Boot application.

--- autodoc-example-java/src/main/java/org/stropa/autodoc/example/AutodocExampleApp.java
+++ autodoc-example-java/src/main/java/org/stropa/autodoc/example/AutodocExampleApp.java
@@ -1,3 +1,5 @@
+package org.stropa.autodoc.example;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
---
+++ autodoc-example-java/src/main/java/org/stropa/autodoc/example/BookingService.java
@@ -0,0 +1,10 @@
+package org.stropa.autodoc.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+
+
+
+}
