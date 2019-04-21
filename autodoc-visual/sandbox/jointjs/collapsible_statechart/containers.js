2efa5b883d7dc3cad5ceaae6a90e6aa50d3e22a4
Maxim Konovalov <maxim.konovalov@ultratendency.com>
2019-04-22 00:45:41 +0300

    WIP collapse/expand groups of elements in jointjs diagram

--- autodoc-visual/sandbox/jointjs/collapsible_statechart/containers.js
+++ autodoc-visual/sandbox/jointjs/collapsible_statechart/containers.js
@@ -2,27 +2,34 @@
 joint.shapes.basic.Generic.define('autodoc.Container', {
     attrs: {
         '.uml-state-body': {
+            'ref':'.uml-state-events', 'refWidth':'120%',
             'width': 200, 'height': 200, 'rx': 0, 'ry': 0,
             'fill': '#ecf0f1', 'stroke': '#bdc3c7', 'stroke-width': 3
         },
         '.uml-state-separator': {
-            'stroke': '#bdc3c7', 'stroke-width': 2
+            'stroke': '#bdc3c7', 'stroke-width': 0
         },
         '.uml-state-name': {
             'ref': '.uml-state-body', 'ref-x': .5, 'ref-y': 5, 'text-anchor': 'middle',
             'fill': '#000000', 'font-family': 'Courier New', 'font-size': 14
         },
+        '.uml-state-header': {
+            'ref':'.uml-state-body',
+            'refWidth':'100%',
+            'width': 20, 'height': 20, fill: 'rgba(48, 208, 198, 0.08)',
+            'stroke': 'rgba(48, 208, 198, 0.4)', 'stroke-width': 2
+        },
         '.uml-state-events': {
-            'ref': '.uml-state-separator', 'ref-x': 5, 'ref-y': 5,
+            'ref': '.uml-state-header', 'ref-x': 5, 'ref-y': '110%', 'refWidth':'100%',
             'fill': '#000000', 'font-family': 'Courier New', 'font-size': 14
         },
-        'tool': {
-            r: 10,
-            cx: 20,
-            cy: 20,
-            fill: 'white',
-            stroke: 'blue',
-            strokeWidth: 2,
+        '.collapse-tool': {
+            r: 6,
+            cx: 10,
+            cy: 10,
+            fill: '#ecf0f1',
+            stroke: '#bdc3c7',
+            strokeWidth: .5,
             cursor  : 'pointer',
             event: 'element:collapse'
         }
@@ -33,28 +40,24 @@ joint.shapes.basic.Generic.define('autodoc.Container', {
 
 }, {
     markup: [
-        '<g class="rotatable">',
-        '<g class="scalable">',
-        '<rect class="uml-state-body"/>',
-        '</g>',
-        '<path class="uml-state-separator"/>',
-        '<text class="uml-state-name"/>',
-        '<text class="uml-state-events"/>',
-        '</g>',
-        '<circle joint-selector="tool" r="30"/>'
+        '<g class="scalable"> \
+            <rect class="uml-state-body"/> \
+        </g> \
+        <rect class="uml-state-header"/>  \
+        <text class="uml-state-name"/> \
+        <text class="uml-state-events"/> \
+        <circle class="collapse-tool" r="10"/>'
     ].join(''),
 
     initialize: function() {
 
         this.on({
             'change:name': this.updateName,
-            'change:events': this.updateEvents,
-            'change:size': this.updatePath
+            'change:events': this.updateEvents
         }, this);
 
         this.updateName();
         this.updateEvents();
-        this.updatePath();
 
         joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments);
     },
@@ -69,13 +72,62 @@ joint.shapes.basic.Generic.define('autodoc.Container', {
         this.attr('.uml-state-events/text', this.get('events').join('\n'));
     },
 
-    updatePath: function() {
-
-        var d = 'M 0 20 L ' + this.get('size').width + ' 20';
-
-        // We are using `silent: true` here because updatePath() is meant to be called
-        // on resize and there's no need to to update the element twice (`change:size`
-        // triggers also an update).
-        this.attr('.uml-state-separator/d', d, { silent: true });
-    }
 });
+
+
+// Collapse / Expand
+function toggleCollapse(group) {
+    var graph = group.graph;
+    if (!graph) return;
+    var embeds;
+    if (group.get('collapsed')) {
+        // EXPAND
+        var subgraph = group.get('subgraph');
+        // deserialize subgraph
+        var tmpGraph = new joint.dia.Graph();
+        tmpGraph.addCells(subgraph);
+        embeds = tmpGraph.getCells();
+        tmpGraph.removeCells(embeds);
+        // set relative position to parent
+        embeds.forEach(function(embed) {
+            if (embed.isLink()) return;
+            var diff = embed.position().offset(group.position());
+            embed.position(diff.x, diff.y);
+        });
+        graph.addCells(embeds);
+        embeds.forEach(function(embed) {
+            if (embed.isElement()) {
+                group.embed(embed);
+            } else {
+                embed.reparent();
+            }
+        });
+        group.set('collapsed', false);
+        //debugger;
+        group.size(group.get('sizeExpanded').width, group.get('sizeExpanded').height)
+        //group.fitEmbeds({ padding: { top: 140, left: 10, right: 10, bottom: 10 }});
+        group.attr('tool/stroke', 'blue');
+
+    } else {
+
+        // COLLAPSE
+        embeds = graph.getSubgraph(group.getEmbeddedCells());
+        embeds.sort(function(a) { return a.isLink() ? 1 : - 1; });
+        graph.removeCells(embeds);
+        // get relative position to parent
+        embeds.forEach(function(embed) {
+            if (embed.isLink()) return;
+            var diff = embed.position().difference(group.position());
+            embed.position(diff.x, diff.y);
+        });
+        // serialize subgraph
+        group.set('sizeExpanded', group.size())
+        group.set('subgraph', embeds.map(function(embed) { return embed.toJSON(); }));
+        group.resize(100, 100);
+        group.set('collapsed', true);
+        group.size(100, 40);
+        group.attr('tool/stroke', 'red');
+    }
+}
+
+
